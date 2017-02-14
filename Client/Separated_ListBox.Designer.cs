d55e31421813003dd13518b0f926e69bbaf94816
Hebatallah <Hebatallah@DESKTOP-J732E08>
2017-02-14 20:57:03 +0200

    Built Login Form and Main Form

---
+++ Client/Separated_ListBox.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Client
+{
+    partial class Separated_ListBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Persons = new System.Windows.Forms.ListBox();
+            this.Difficulty_Level = new System.Windows.Forms.ListBox();
+            this.Category = new System.Windows.Forms.ListBox();
+            this.Rooms = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(279, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Persons";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(176, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Difficulty_Level";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Category";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Rooms";
+            // 
+            // Persons
+            // 
+            this.Persons.FormattingEnabled = true;
+            this.Persons.Items.AddRange(new object[] {
+            "1/2",
+            "2/2",
+            "2/2",
+            "1/2"});
+            this.Persons.Location = new System.Drawing.Point(261, 54);
+            this.Persons.Name = "Persons";
+            this.Persons.Size = new System.Drawing.Size(80, 186);
+            this.Persons.TabIndex = 12;
+            this.Persons.SelectedIndexChanged += new System.EventHandler(this.Rooms_SelectedIndexChanged);
+            // 
+            // Difficulty_Level
+            // 
+            this.Difficulty_Level.FormattingEnabled = true;
+            this.Difficulty_Level.Items.AddRange(new object[] {
+            "medium",
+            "hard",
+            "easy",
+            "medium"});
+            this.Difficulty_Level.Location = new System.Drawing.Point(176, 54);
+            this.Difficulty_Level.Name = "Difficulty_Level";
+            this.Difficulty_Level.Size = new System.Drawing.Size(80, 186);
+            this.Difficulty_Level.TabIndex = 9;
+            this.Difficulty_Level.SelectedIndexChanged += new System.EventHandler(this.Rooms_SelectedIndexChanged);
+            // 
+            // Category
+            // 
+            this.Category.FormattingEnabled = true;
+            this.Category.Items.AddRange(new object[] {
+            "cat1",
+            "cat2",
+            "cat3",
+            "cat4"});
+            this.Category.Location = new System.Drawing.Point(92, 54);
+            this.Category.Name = "Category";
+            this.Category.Size = new System.Drawing.Size(80, 186);
+            this.Category.TabIndex = 10;
+            this.Category.SelectedIndexChanged += new System.EventHandler(this.Rooms_SelectedIndexChanged);
+            // 
+            // Rooms
+            // 
+            this.Rooms.FormattingEnabled = true;
+            this.Rooms.Items.AddRange(new object[] {
+            "Room 1",
+            "Room 2",
+            "Room 3",
+            "Room 4"});
+            this.Rooms.Location = new System.Drawing.Point(9, 54);
+            this.Rooms.Name = "Rooms";
+            this.Rooms.Size = new System.Drawing.Size(80, 186);
+            this.Rooms.TabIndex = 11;
+            this.Rooms.SelectedIndexChanged += new System.EventHandler(this.Rooms_SelectedIndexChanged);
+            // 
+            // Separated_ListBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Persons);
+            this.Controls.Add(this.Difficulty_Level);
+            this.Controls.Add(this.Category);
+            this.Controls.Add(this.Rooms);
+            this.Name = "Separated_ListBox";
+            this.Size = new System.Drawing.Size(362, 291);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox Persons;
+        private System.Windows.Forms.ListBox Difficulty_Level;
+        private System.Windows.Forms.ListBox Category;
+        private System.Windows.Forms.ListBox Rooms;
+    }
+}
---
+++ Client/login.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Client
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Login_btn = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.my_TextBox1 = new My_TextBox.My_TextBox();
+            this.SuspendLayout();
+            // 
+            // Login_btn
+            // 
+            this.Login_btn.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Login_btn.Location = new System.Drawing.Point(106, 89);
+            this.Login_btn.Name = "Login_btn";
+            this.Login_btn.Size = new System.Drawing.Size(70, 25);
+            this.Login_btn.TabIndex = 2;
+            this.Login_btn.Text = "Login";
+            this.Login_btn.UseVisualStyleBackColor = true;
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(182, 89);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(70, 25);
+            this.Exit.TabIndex = 4;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // my_TextBox1
+            // 
+            this.my_TextBox1.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Italic);
+            this.my_TextBox1.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.my_TextBox1.Location = new System.Drawing.Point(79, 48);
+            this.my_TextBox1.Name = "my_TextBox1";
+            this.my_TextBox1.PlaceHolder = "Username";
+            this.my_TextBox1.Size = new System.Drawing.Size(200, 24);
+            this.my_TextBox1.TabIndex = 3;
+            this.my_TextBox1.Text = "Username";
+            // 
+            // Login
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.ClientSize = new System.Drawing.Size(373, 154);
+            this.ControlBox = false;
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.my_TextBox1);
+            this.Controls.Add(this.Login_btn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Login";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login Form";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Login_btn;
+        private My_TextBox.My_TextBox my_TextBox1;
+        private System.Windows.Forms.Button Exit;
+    }
+}
