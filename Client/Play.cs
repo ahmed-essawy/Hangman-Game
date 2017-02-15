d2ec39082bb150c20e67287c56894d90767e29c5
Donna <madonnaibrahim@yahoo.com>
2017-02-15 12:10:52 +0200

    play form

---
+++ Client/Play.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Play : Form
+    {
+        public Play()
+        {
+            InitializeComponent();
+        }
+
+        private void panel_labels_Paint(object sender, PaintEventArgs e)
+        {
+
+            int ControlNum = 30;
+            Label[] lbl_array = new Label[ControlNum];
+            for (int i = 0, locx = 10, locy = 30; i < ControlNum; i++, locx += 30)
+            {
+                lbl_array[i] = new Label();
+                lbl_array[i].Name = "lblName" + i;
+                lbl_array[i].Size = new Size(10, 10);
+                if (locx < this.Width - 50)
+                {
+                    lbl_array[i].Location = new Point(locx, locy);
+                }
+                else
+                {
+                    locy += 50;
+                    if (locx / 30 != 1)
+                    {
+                        locx = 10;
+                        lbl_array[i].Location = new Point(locx, locy);
+                    }
+                }
+                lbl_array[i].Text = "-";
+                panel_labels.Controls.Add(lbl_array[i]);
+
+
+            }
+        }
+
+        private void Play_Load(object sender, EventArgs e)
+        {
+            panel_Alpha.Location = new Point(panel_labels.Location.X, panel_labels.Size.Height + 30);
+        }
+
+        private void button_rules_Click(object sender, EventArgs e)
+        {
+            rules rule = new rules();
+            DialogResult d = rule.ShowDialog();
+
+            if (d == DialogResult.OK)
+            {
+            }
+        }
+    }
+}
---
+++ Client/rules.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class rules : Form
+    {
+        string cat;
+
+        int level;
+       
+
+        public string Cat
+        {
+            get
+            {
+                return cat;
+            }
+
+            set
+            {
+                cat = value;
+            }
+        }
+
+        public int Level
+        {
+            get
+            {
+                return level;
+            }
+
+            set
+            {
+                level = value;
+            }
+        }
+
+        public rules()
+        {
+            InitializeComponent();
+        }
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            Cat = comboBox_cat.SelectedItem.ToString();
+            Level =int.Parse( comboBox_level.SelectedItem.ToString());
+        }
+    }
+}
