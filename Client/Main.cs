4740d9bcca2d8d2bc1bec7cb16f7a65aaa433349
Ahmed El-Essawy <ahm.elessawy@gmail.com>
2017-02-17 03:42:11 +0200

    Merge files

--- Client/Main.cs
+++ Client/Main.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
@@ -13,14 +14,20 @@ namespace Client
     public partial class Main : Form
     {
         private string Username;
+        private int endpoint;
         private NetworkStream nStream;
+        private BinaryReader breader;
+        private BinaryWriter bwriter;
 
         public Main(NetworkStream nStream, String Username)
         {
             InitializeComponent();
             this.nStream = nStream;
             this.Username = Username;
-            //MessageBox.Show(Username);
+            bwriter = new BinaryWriter(nStream);
+            breader = new BinaryReader(nStream);
+            bwriter.Write(Username);
+            endpoint = int.Parse(breader.ReadString());
             Play.Enabled = false;
             Watch.Enabled = false;
             CategList.Enabled = false;
@@ -32,8 +39,8 @@ namespace Client
         {
             Play game = new Play();
             game.Show();
-
         }
+
         private void RoomList_SelectedIndexChanged(object sender, EventArgs e)
         {
             int index = ((ListBox)sender).SelectedIndex;
@@ -41,7 +48,7 @@ namespace Client
             CategList.SelectedIndex = index;
             DiffList.SelectedIndex = index;
             PersonList.SelectedIndex = index;
-            string person =PersonList.SelectedItem.ToString();
+            string person = PersonList.SelectedItem.ToString();
             if (person == "2/2") { Watch.Enabled = true; Play.Enabled = false; }
             else { Watch.Enabled = false; Play.Enabled = true; }
         }
@@ -50,12 +57,12 @@ namespace Client
         {
             Rules NewRules = new Rules();
             NewRules.ShowDialog();
-            string Category=string.Empty;
+            string Category = string.Empty;
             int Level = 0, x = 1000;
             if (NewRules.DialogResult == DialogResult.OK)
             {
                 Category = NewRules.Cat;
-                 Level = NewRules.Level;
+                Level = NewRules.Level;
             }
             string send = "newroom;" + x + ";" + Category + ";" + Level.ToString();
         }
